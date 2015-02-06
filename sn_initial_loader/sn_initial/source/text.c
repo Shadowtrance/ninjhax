704e74d04a45bdba72ff7739e81c7d0984bc4335
Yifan Lu <me@yifanlu.com>
2015-02-05 22:40:29 -0800

    Added draw printf to screen for debugging

--- sn_initial_loader/sn_initial/source/text.c
+++ sn_initial_loader/sn_initial/source/text.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,6 +10,166 @@
 #define CHAR_SIZE_Y (8)
 
 int _strlen(char* str);
+
+/** \brief Division result
+ *  \sa uidiv
+ */
+typedef struct uidiv_result {
+    u32 quo;  ///< Quotient
+    u32 rem;  ///< Remainder
+} uidiv_result_t;
+
+/********************************************//**
+ *  \brief Unsigned integer division
+ *  
+ *  ARM does not have native division support
+ *  \returns Result of operation or zero if 
+ *  dividing by zero.
+ ***********************************************/
+uidiv_result_t
+uidiv (u32 num,   ///< Numerator
+       u32 dem)   ///< Denominator
+{
+    u32 tmp = dem;
+    uidiv_result_t ans = {0};
+    
+    if (dem == 0)
+    {
+        // TODO: Somehow make error
+        return ans;
+    }
+    
+    while (tmp <= num >> 1)
+    {
+        tmp <<= 1;
+    }
+    
+    do
+    {
+        if (num >= tmp)
+        {
+            num -= tmp;
+            ans.quo++;
+        }
+        ans.quo <<= 1;
+        tmp >>= 1;
+    } while (tmp >= dem);
+    ans.quo >>= 1;
+    ans.rem = num;
+    
+    return ans;
+}
+
+// thanks naehrwert for the tiny printf
+static void _putn(char **p_str, u32 x, u32 base, char fill, int fcnt, int upper)
+{
+    char buf[65];
+    char *digits;
+    char *p;
+    int c = fcnt;
+    uidiv_result_t div_res;
+
+    if (upper)
+        digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    else
+        digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+    
+    if(base > 36)
+        return;
+
+    p = buf + 64;
+    *p = 0;
+    do
+    {
+        c--;
+        div_res = uidiv (x, base);
+        *--p = digits[div_res.rem];
+        x = div_res.quo;
+    }while(x);
+    
+    if(fill != 0)
+    {
+        while(c > 0)
+        {
+            *--p = fill;
+            c--;
+        }
+    }
+    
+    for(; *p != '\0'; *((*p_str)++) = *(p++));
+}
+
+/********************************************//**
+ *  \brief Simple @c vsprintf
+ *  
+ *  Only supports %c, %s, %u, %x, %X with 
+ *  optional zero padding.
+ *  Always returns zero.
+ ***********************************************/
+int _vsprintf (char *str, const char *fmt, va_list ap)
+{
+    char *s;
+    char c, fill;
+    int fcnt;
+    u32 n;
+    
+    while(*fmt)
+    {
+        if(*fmt == '%')
+        {
+            fmt++;
+            fill = 0;
+            fcnt = 0;
+            if((*fmt >= '0' && *fmt <= '9') || *fmt == ' ')
+                if(*(fmt+1) >= '0' && *(fmt+1) <= '9')
+                {
+                    fill = *fmt;
+                    fcnt = *(fmt+1) - '0';
+                    fmt++;
+                    fmt++;
+                }
+            switch(*fmt)
+            {
+            case 'c':
+                c = va_arg(ap, u32);
+                *(str++) = c;
+                break;
+            case 's':
+                s = va_arg(ap, char *);
+                for(; *s != '\0'; *(str++) = *(s++));
+                break;
+            case 'u':
+                n = va_arg(ap, u32);
+                _putn(&str, n, 10, fill, fcnt, 0);
+                break;
+            case 'x':
+                n = va_arg(ap, u32);
+                _putn(&str, n, 16, fill, fcnt, 0);
+                break;
+            case 'X':
+                n = va_arg(ap, u32);
+                _putn(&str, n, 16, fill, fcnt, 1);
+                break;
+            case '%':
+                *(str++) = '%';
+                break;
+            case '\0':
+                goto out;
+            default:
+                *(str++) = '%';
+                *(str++) = *fmt;
+                break;
+            }
+        }
+        else
+            *(str++) = *fmt;
+        fmt++;
+    }
+
+    out:
+    *str = '\0';
+    return 0;
+}
 
 void drawCharacter(u8* fb, char c, u16 x, u16 y)
 {
@@ -43,4 +204,23 @@ void drawString(u8* fb, char* str, u16 x, u16 y)
 		dx+=8;
 		if(str[k]=='\n'){dx=0;dy-=8;}
 	}
+}
+
+/********************************************//**
+ *  \brief Simple @c sprintf
+ *  
+ *  Only supports %c, %s, %u, %x, %X with 
+ *  optional zero padding. 1024 max length.
+ *  Always returns zero.
+ ***********************************************/
+void drawFormat(u8* fb, u16 x, u16 y, const char *format, ...)
+{
+    char buffer[1024];
+    va_list arg;
+
+    va_start (arg, format);
+    _vsprintf (buffer, format, arg);
+    va_end (arg);
+
+    drawString (fb, buffer, x, y);
 }
--- sn_initial_loader/sn_initial/source/text.h
+++ sn_initial_loader/sn_initial/source/text.h
@@ -3,5 +3,6 @@
 
 void drawCharacter(u8* fb, char c, u16 x, u16 y);
 void drawString(u8* fb, char* str, u16 x, u16 y);
+void drawFormat(u8* fb, u16 x, u16 y, const char *format, ...);
 
 #endif
